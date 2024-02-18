52bb590d74c81764fb76e3c2b5e1a505cf79dfa7
Priya Kumari <priya.official2024@gmail.com>
2024-02-18 17:26:54 +0530

    added resume

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Particle from '../Particle';
-import pdf from '../../Assets/../Assets/Priya-feb Resume.pdf';
+import pdf from '../../Assets/../Assets/Priyawebsite.pdf';
 import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
